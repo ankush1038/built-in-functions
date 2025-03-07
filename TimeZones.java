970c5c62c098500ee7406ed58fee255da3d629a9
ankush1038 <ankusharma1029@gmail.com>
2025-03-07 14:07:39 +0530

    [Ankush] Add: Time zones of desired inputs are printed

---
+++ TimeZones.java
@@ -0,0 +1,18 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+
+public class TimeZones {
+
+    public static void main(String[] args) {
+        
+        // Get current time in different time zones
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        
+        // Display time in different time zones
+        System.out.println("Current Time in GMT: " + gmtTime);
+        System.out.println("Current Time in IST: " + istTime);
+        System.out.println("Current Time in PST: " + pstTime);
+    }
+}
