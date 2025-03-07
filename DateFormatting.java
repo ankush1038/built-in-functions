b116d6d697d90970bee8895305bb34576d45110a
ankush1038 <ankusharma1029@gmail.com>
2025-03-07 14:20:11 +0530

    [Ankush] Add: Date and time printed as asked

---
+++ DateFormatting.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DateFormatting {
+
+    public static void main(String[] args) {
+        
+        // Get current date
+        LocalDate currentDate = LocalDate.now();
+        
+        // Define different date formats
+        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter format2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter format3 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+        
+        // Display date in different formats
+        System.out.println("Date in format dd/MM/yyyy: " + currentDate.format(format1));
+        System.out.println("Date in format yyyy-MM-dd: " + currentDate.format(format2));
+        System.out.println("Date in format EEE, MMM dd, yyyy: " + currentDate.format(format3));
+    }
+}
